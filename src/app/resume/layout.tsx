068615aae62a821706dafa35fdecdb0b606903fa
Haegnim <haegnim@gmail.com>
2023-12-05 19:46:43 +0900

    feat : favicon

--- src/app/resume/layout.tsx
+++ src/app/resume/layout.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const layout = () => {
-  return <div>layout</div>;
+  return (
+    // <div style={{ display: "flex" }}>
+    <>
+      <div
+        className="test"
+        style={{ display: "inline-block", border: "5px solid " }}
+      >
+        layout
+      </div>
+      <div className="test" style={{ display: "inline", border: "5px solid " }}>
+        layout
+      </div>
+      <div className="test" style={{ display: "block", border: "5px solid " }}>
+        layout
+      </div>
+    </>
+    // </div>
+  );
 };
 
 export default layout;
